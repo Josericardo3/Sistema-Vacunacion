a961b20f1547f8df63f2f9c1e0df4914305a9182
Josericardo3 <jose.caicedo1@unmsm.edu.pe>
2021-01-07 14:35:41 -0500

    actualizacion-vistas

--- resources/views/mayor.blade.php
+++ resources/views/mayor.blade.php
@@ -2,34 +2,25 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Registro</title>
-    <link href="{{ asset('css/iniciosesion.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/detalles.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>Mayores de 60 años</title>
+    <link href="{{ asset('css/consulta.css') }}" rel="stylesheet">
   </head>
   <body>
-  <button class="btn btn-info m-3"><a id="enlace" href="{{ url("/inicioR") }}">Atrás</a></button>
-<div class="container">  
-  <div class="row">
-    <div class="col-4"></div>
-    <div class="col-4">
-      <form id="contact" action="" method="post">
-        <h3>Mayores de 60 años</h3>
-        <br>
-        <fieldset>
-          <input placeholder="DNI" type="text" tabindex="1" required autofocus>
-        </fieldset>
-        <fieldset>
-          <input placeholder="Fecha de Emisión del DNI" type="email" tabindex="2" required>
-        </fieldset>
-        
-        <fieldset>
-          <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Ingresar</button>
-        </fieldset>
-      </form>
-    </div>
-    <div class="col-4"></div>
-  </div>
+    <div class="login-box">
+      <h1>Mayores de 60 años</h1>
+      <div class="object"></div>
+      <form>    
+    <div class="container">  
+      <br>
+      <!-- N° de dni -->
+      <label for="number">Número de DNI</label>
+      <input type="text" placeholder="Ingrese su número de DNI">
+      <a href="detalles">
+        <input type="button" value="Ingresar">
+      </a>
+      
+    </form>
+   
   </div>
 </body>
 </html>
